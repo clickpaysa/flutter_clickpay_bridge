90ae0604d44d18038d40e027d31a0983a0a9c0ce
Mohamed Abdelbasit <mohamednagdy257@gmail.com>
2025-05-26 11:33:49 +0300

    1- Fix andorid issue with flutter 3.29 with registrar 2- update example project to flutter 3.29 3- try the example project with flutter 3.29 4- update the pubspec.lock 5- update the example/ios/Podfile.lock 6- update the example/ios/Runner.xcodeproj/project.pbxproj 7- update the example/ios/Runner.xcodeproj/xcshareddata/xcschemes/Runner.xcscheme 8- update the example/ios/Runner/AppDelegate.swift 9- update the example/pubspec.lock 10- update the pubspec.lock

--- android/src/main/java/com/paymentsdk/flutter_paymentsdk_bridge/FlutterPaymentSdkBridgePlugin.java
+++ android/src/main/java/com/paymentsdk/flutter_paymentsdk_bridge/FlutterPaymentSdkBridgePlugin.java
@@ -45,7 +45,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import static com.payment.paymentsdk.integrationmodels.PaymentSdkApmsKt.createPaymentSdkApms;
 import static com.payment.paymentsdk.integrationmodels.PaymentSdkLanguageCodeKt.createPaymentSdkLanguageCode;
@@ -90,20 +89,6 @@ public class FlutterPaymentSdkBridgePlugin implements FlutterPlugin, MethodCallH
                     }
                 }
         );
-    }
-
-    // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-    // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-    // plugin registration via this function while apps migrate to use the new Android APIs
-    // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-    //
-    // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-    // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-    // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-    // in the same class.
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_payment_sdk_bridge_emulator");
-        channel.setMethodCallHandler(new FlutterPaymentSdkBridgePlugin());
     }
 
     @Override
